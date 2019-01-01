7b0a8cc4a3b436d16b4de2e0c45f35e5698f3133
Norbert Klasen <klasen@gmx.net>
2019-01-01 18:06:16 +0100

    report touch poind via mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,9 @@
-/***************************************************
-  This is our touchscreen painting example for the Adafruit ILI9341 Shield
-  ----> http://www.adafruit.com/products/1651
-
-  Check out the links above for our tutorials and wiring diagrams
-  These displays use SPI to communicate, 4 or 5 pins are required to
-  interface (RST is optional)
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- ****************************************************/
+//Define DEBUG to get the Output from DEBUG_PRINTLN
+#define DEBUG 1
 
 #include <SPI.h>
-//#include <Wire.h>      // this is needed even tho we aren't using it
 #include <Adafruit_ILI9341.h>
+
 #include <XPT2046_Touchscreen.h>
 
 //Include Basecamp in this sketch
@@ -23,7 +11,7 @@
 #include <Configuration.hpp>
 
 //Basecamp iot{Basecamp::SetupModeWifiEncryption::secured, Basecamp::ConfigurationUI::accessPoint};
-Basecamp iot;
+Basecamp iot(Basecamp::SetupModeWifiEncryption::none, Basecamp::ConfigurationUI::always);
 
 // This is calibration data for the raw touch data to the screen coordinates
 #define TS_MINX 450
@@ -33,6 +21,7 @@ Basecamp iot;
 
 // The XPT204 uses hardware SPI
 #define TS_CS 22
+#define TS_IRQ 3
 XPT2046_Touchscreen ts = XPT2046_Touchscreen(TS_CS);
 
 // The display also uses hardware SPI
@@ -47,6 +36,96 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RESET);
 #define PENRADIUS 3
 int oldcolor, currentcolor, brightness;
 
+TS_Point p_old = TS_Point();
+
+//This is used to control if the ESP should enter sleep mode or not
+bool delaySleep = false;
+
+//Variables for the mqtt packages and topics
+uint16_t statusPacketIdSub = 0;
+String delaySleepTopic;
+String statusTopic;
+String touchPointTopic;
+
+// Reset the configuration to factory defaults (all empty)
+void resetToFactoryDefaults()
+{
+  DEBUG_PRINTLN("Resetting to factory defaults");
+  Configuration config(String{"/basecamp.json"});
+  config.load();
+  config.resetExcept({
+      ConfigurationKey::accessPointSecret,
+  });
+  config.save();
+}
+
+//This function transfers the state of the sensor. That includes the door status, battery status and level
+void transmitStatus()
+{
+  DEBUG_PRINTLN(__func__);
+
+  char sensorC[18];
+  //convert the sensor value to a string
+  sprintf(sensorC, "%04i %04i %04i", p_old.x, p_old.y, p_old.z);
+  //Send the sensor value to the MQTT broker
+  iot.mqtt.publish(touchPointTopic.c_str(), 1, true, sensorC);
+
+  DEBUG_PRINTLN("Data published");
+}
+
+//This function is called when the MQTT-Server is connected
+void onMqttConnect(bool sessionPresent)
+{
+  DEBUG_PRINTLN(__func__);
+  DEBUG_PRINTLN(iot.hostname);
+  DEBUG_PRINTLN(iot.wifi.getIP().toString());
+  
+
+  //Subscribe to the delay topic
+  iot.mqtt.subscribe(delaySleepTopic.c_str(), 0);
+  //Trigger the transmission of the current state.
+  transmitStatus();
+}
+
+
+//This topic is called if an MQTT message is received
+void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
+{
+  DEBUG_PRINTLN(__func__);
+
+  //Check if the payload eqals "true" and set delaySleep correspondigly
+  //Since we only subscribed to one topic, we only have to compare the payload
+  if (strcmp(payload, "true") == 0)
+  {
+    delaySleep = true;
+  }
+  else
+  {
+    delaySleep = false;
+  }
+}
+
+void suspendESP(uint16_t packetId)
+{
+  DEBUG_PRINTLN(__func__);
+
+  //Check if the published package is the one of the door sensor
+  if (packetId == statusPacketIdSub)
+  {
+
+    if (delaySleep == true)
+    {
+      DEBUG_PRINTLN("Delaying Sleep");
+      return;
+    }
+    DEBUG_PRINTLN("Entering deep sleep");
+    //properly disconnect from the MQTT broker
+    iot.mqtt.disconnect();
+    //send the ESP into deep sleep
+    esp_deep_sleep_start();
+  }
+}
+
 void setup(void)
 {
   // while (!Serial);     // used for leonardo debugging
@@ -56,6 +135,21 @@ void setup(void)
 
   // Initialize Basecamp
   iot.begin();
+  // iot.wifi.begin("WLAN-Grundmann", "0043847185725197", "True", "WateringESP", "");
+  // iot.mqtt.setServer("172.30.1.12", 1883);
+
+  //Configure the MQTT topics
+  delaySleepTopic = "cmd/" + iot.hostname + "/delaysleep";
+  statusTopic = "stat/" + iot.hostname + "/status";
+  touchPointTopic = "stat/" + iot.hostname + "/touchpoint";
+
+  //Set up the Callbacks for the MQTT instance. Refer to the Async MQTT Client documentation
+  // TODO: We should do this actually _before_ connecting the mqtt client...
+  iot.mqtt.onConnect(onMqttConnect);
+  // iot.mqtt.onPublish(suspendESP);
+  iot.mqtt.onMessage(onMqttMessage);
+
+  iot.mqtt.connect();
 
   tft.begin();
   tft.setRotation(1);
@@ -109,7 +203,20 @@ void setup(void)
   //pinMode(TFT_LED, OUTPUT);
   brightness = 100;
   dacWrite(DAC1, brightness);
+
+  p_old = ts.getPoint();
+
+
+  
+  Serial.print("wifi: ");
+  Serial.print(iot.wifi.status());
+  Serial.print(", wifi: ");
+  // Serial.print(iot.wifi.;
+  // Serial.print(", wifi: ");
+  Serial.print(iot.wifi.getIP().toString());
+  Serial.println();
 }
+
 
 void loop()
 {
@@ -120,26 +227,13 @@ void loop()
     return;
   }
 
-  // You can also wait for a touch
-  if (!ts.touched())
-  {
-    if (brightness < 70)
-    {
-      brightness++;
-      dacWrite(DAC1, brightness);
-      delay(20);
-    }
-    else
-    {
-      brightness = 0;
-    }
-    Serial.print("brightness = ");
-    Serial.println(brightness);
-    return;
-  }
-
   // Retrieve a point
   TS_Point p = ts.getPoint();
+  if (p == p_old)
+  {
+    return;
+  }
+  p_old = p;
 
   Serial.print("X = ");
   Serial.print(p.x);
@@ -147,6 +241,8 @@ void loop()
   Serial.print(p.y);
   Serial.print("\tPressure = ");
   Serial.print(p.z);
+
+  transmitStatus();
 
   // Scale from ~0->4000 to tft.width using the calibration #'s
   p.x = map(TS_MAXX - p.x, TS_MINX, TS_MAXX, 0, tft.width()) + 40;
